2227894254b6c7f324f30fedb0eb3fb3926a2478
Anna Kravets <annakravets1906@gmail.com>
2024-09-27 21:24:16 +0300

    Added build for node.js

--- frontend/src/redux/events/eventsApi.js
+++ frontend/src/redux/events/eventsApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL =
-  'https://events-registration-app-backend-5zhzrrrfz-an-nettts-projects.vercel.app';
+  'https://events-registration-app-backend-4qf1sb2nu-an-nettts-projects.vercel.app';
 
 export const eventsAPI = createAsyncThunk(
   'events/fetchAll',
