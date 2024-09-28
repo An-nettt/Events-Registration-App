160c5648312d828a6b2723fdf4e6ab9b91839283
Anna Kravets <annakravets1906@gmail.com>
2024-09-28 10:34:50 +0300

    Added corsOptions for Options method

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,14 @@ import usersRouter from '../api/users-router.js';
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan('dev'));
 
